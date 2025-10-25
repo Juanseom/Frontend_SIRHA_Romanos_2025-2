8a37d7b86ffa2f108f20cee62d9c45dc24d4762a
mbquial <pepitotolitis@gmail.com>
2025-10-25 18:43:18 -0500

    feat: crear pantallas home de admin y student

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom'
+
+// Dean routes
 import DeanHome from './dean/DeanHome'
 import GestionarSolicitudes from './dean/GestionarSolicitudes'
 import Periodos from './dean/Periodos'
 import MonitorearGrupos from './dean/MonitorearGrupos'
 
+// Student routes
+import StudentHome from './student/StudentHome'
+
+// Admin routes
+import AdminHome from './admin/AdminHome'
+
 export const router = createBrowserRouter([
+  // Dean routes
   {
     path: '/dean-home',
     element: <DeanHome />,
@@ -21,6 +30,28 @@ export const router = createBrowserRouter([
     path: '/dean/monitorear-grupos',
     element: <MonitorearGrupos />,
   },
+
+  // Student routes
+  {
+    path: '/student-home',
+    element: <StudentHome />,
+  },
+  // {
+  //   path: '/student/mis-solicitudes',
+  //   element: <MisSolicitudes />,
+  // },
+
+  // Admin routes
+  {
+    path: '/admin-home',
+    element: <AdminHome />,
+  },
+  // {
+  //   path: '/admin/gestionar-solicitudes',
+  //   element: <AdminGestionarSolicitudes />,
+  // },
+
+  // Default redirect
   {
     path: '/',
     element: <DeanHome />,
